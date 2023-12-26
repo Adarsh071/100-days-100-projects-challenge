8d40049ba7e69cfacd5a664e83c6134645bf0e65
Adarsh071 <adarshkumarkas@gmail.com>
2023-12-26 11:38:47 +0530

    first commit

---
+++ project-32 Inc. and Dec/index.js
@@ -0,0 +1,27 @@
+const countValue = document.querySelector('#counter');
+
+const increment = () => {
+    // get the value from UI
+    let value = parseInt(countValue.innerText);
+    // Update the value
+    value = value + 1;
+    // Set the value onto UI
+    countValue.innerText = value;
+};
+
+const decrement = () => {
+    // get the value from UI
+    let value = parseInt(countValue.innerText);
+    // Update the value
+    value = value - 1;
+    // Set the value onto UI
+    countValue.innerText = value;
+};
+
+
+
+
+
+
+
+
