26fa8e7254e8c4bd188f8dac514f8ab9133ec3d8
Adarsh071 <adarshkumarkas@gmail.com>
2024-01-19 21:28:58 +0530

    first commit

---
+++ project-49 Temperature Converter/script.js
@@ -0,0 +1,23 @@
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelcius").innerHTML= (valNum-32)/1.8;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
